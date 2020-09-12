cf52d84cfb77e081e4a76acca77a501c259f2b3a
unknown <anthony.abramo@epitech.eu>
2020-09-12 18:11:21 +0200

    modified for Homepage setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import LoginPanel from './components/LoginPanel';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPanel />
+      <Homepage />
     </div>
   );
 }
