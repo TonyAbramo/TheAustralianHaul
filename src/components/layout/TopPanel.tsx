5557541a0860a4a869e72ecd47aeec8287527cdb
Victor Toupar <victor.toupar@gmail.com>
2020-10-03 21:46:00 +0800

    EsLinter

--- src/components/layout/TopPanel.tsx
+++ src/components/layout/TopPanel.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import NavBar from '../menu/NavBar';
 
-export default function TopPanel () {
-    
-const topPanelStyle: React.CSSProperties = {
-    backgroundColor: "#AAD7E6",
-    height:"50%",
-    maxHeight:"50%",
-    width:"100%",
-    position:"absolute"
-}
+export default function TopPanel() {
 
-    return(
+    const topPanelStyle: React.CSSProperties = {
+        backgroundColor: "#AAD7E6",
+        height: "50%",
+        maxHeight: "50%",
+        width: "100%",
+        position: "absolute"
+    }
+
+    return (
         <div style={topPanelStyle} >
             <NavBar />
         </div>
