643926b21013a48153f946942f96a2eace84a11f
Anthony ABRAMO <anthony.abramo@epitech.eu>
2020-10-03 12:47:01 +0200

    [Experiment][Bugfix] Fix height main container

--- src/components/layout/Homepage.tsx
+++ src/components/layout/Homepage.tsx
@@ -9,7 +9,7 @@ export default function Homepage() {
     left: 0,
     right: 0,
     bottom: 0,
-    height: 750,
+    height: "90%",
     width: "70%",
     backgroundColor: "#eeeeee",
     margin: "auto",
